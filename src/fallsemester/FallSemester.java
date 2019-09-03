8d6ed70f94a3ebb98b119c16960fd705a80c5219
Shemar Woodley <shemarwoodley@yahoo.com>
2019-09-03 18:37:34 -0400

    Class one - String Manipulation

---
+++ src/fallsemester/FallSemester.java
@@ -0,0 +1,16 @@
+
+package fallsemester;
+
+public class FallSemester {
+
+    public static void main(String[] args) {
+        
+        String string1 = new String ("Shemar");
+        String string2 = new String ("Shemar");
+        
+        System.out.println(string1 == string2);
+        System.out.println(string1.equals(string2));
+        // TODO code application logic here
+    }
+    
+}
